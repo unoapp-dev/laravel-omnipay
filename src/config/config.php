7a3739ba21e55bf452ccfb834f2baae4c8cb4220
Anil Ahir <anilahir@unoindia.co>
2018-08-17 11:35:52 +0530

    sample config updated

--- src/config/config.php
+++ src/config/config.php
@@ -2,19 +2,48 @@
 
 return [
 
-	// The default gateway to use
-	'default' => 'paypal',
+    // The default gateway to use
+    'default' => 'beanstream',
 
-	// Add in each gateway here
-	'gateways' => [
-		'paypal' => [
-			'driver'  => 'PayPal_Express',
-			'options' => [
-				'solutionType'   => '',
-				'landingPage'    => '',
-				'headerImageUrl' => ''
-			]
-		]
-	]
+    // Add in each gateway here
+    'gateways' => [
 
+        'beanstream' => [
+            'driver' => 'Beanstream',
+            'options' => [
+                'merchantId' => env('BEANSTREAM_MERCHANT_ID', ''),
+                'profilePasscode' => env('BEANSTREAM_PROFILE_PASSCODE', ''),
+                'transactionPasscode' => env('BEANSTREAM_TRANSACTION_PASSCODE', ''),
+                'testMode' => env('BEANSTREAM_TEST_MODE', '')
+            ]
+        ],
+
+        'moneris' => [
+            'driver' => 'Moneris',
+            'options' => [
+                'merchantId' => env('MONERIS_MERCHANT_ID', ''),
+                'merchantKey' => env('MONERIS_MERCHANT_KEY', ''),
+                'testMode' => env('MONERIS_TEST_MODE', '')
+            ]
+        ],
+
+        'vantiv' => [
+            'driver' => 'Vantiv',
+            'options' => [
+                'username' => env('VANTIV_USERNAME', ''),
+                'password' => env('VANTIV_PASSWORD', ''),
+                'testMode' => env('VANTIV_TEST_MODE', '')
+            ]
+        ],
+
+        'payeezy' => [
+            'driver' => 'Payeezy',
+            'options' => [
+                'apiKey' => env('PAYEEZY_API_KEY', ''),
+                'apiSecret' => env('PAYEEZY_API_SECRET', ''),
+                'merchantToken' => env('PAYEEZY_MERCHANT_TOKEN', ''),
+                'testMode' => env('PAYEEZY_TEST_MODE', '')
+            ]
+        ]
+    ]
 ];
